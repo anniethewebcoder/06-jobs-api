f3b753a1b2b4f1b3d2d729dc4397575c273c349b
anniethewebcoder <anniethewebcoder@gmail.com>
2024-05-08 17:36:36 -0400

    job part 1

---
+++ middleware/authentication.js
@@ -0,0 +1,31 @@
+const User = require('./../models/User')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('./../errors')
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if(!authorization || !authHeader.startsWith('Bearer')) {
+        throw new UnauthenticatedError('Authentication Invalid')
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = User.findById(payload.id).select('-password')
+        req.user = user
+        
+        req.user = {
+            userId: payload.userId,
+            name: payload.name
+        }
+
+        next()
+    } catch(error) {
+        throw new UnauthenticatedError('Authenticaion Invalid')
+    }
+}
+
+module.exports = auth
