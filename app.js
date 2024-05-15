5b5d93ca84ceab4ff4ae1c9f5b6da6a50f496ec9
anniethewebcoder <anniethewebcoder@gmail.com>
2024-05-15 13:55:56 -0400

    cors

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -13,9 +19,12 @@ const jobsRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const { RateLimiter } = require('rate-limiter');
 
 app.use(express.json());
-// extra packages
+app.use(cors())
+app.use(xss())
+app.use(RateLimiter)
 
 // routes
 app.use('/api/v1/auth', authRouter)
