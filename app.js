7af097855aa9c9c142a8bb87ce51e55be5d2cee3
anniethewebcoder <anniethewebcoder@gmail.com>
2025-02-07 00:39:41 -0500

    routes

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
