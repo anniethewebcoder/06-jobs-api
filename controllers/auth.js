809d80fdd44399639e38bc94aa15f50fc6c07e10
anniethewebcoder <anniethewebcoder@gmail.com>
2025-02-08 22:21:07 -0500

    week9

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,44 @@
-const User = require('./../models/User')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('./../errors')
-const bcrypt = require('bcryptjs')
+const User = require("./../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("./../errors");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  const tempUser = { name, email, password: hashedPassword }
-
-  // if(!name || !email || !password) {
-  //   throw new BadRequestError('Please provide name, email, and password.')
-  // }
-
   const user = await User.create({
-    ...req.body
-  })
-  res.status(StatusCodes.CREATED).json({ tempUser });
+    ...req.body,
+  });
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({
+    user: { name: user.name },
+    token,
+  });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalide Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalide Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: { name: user.name },
+    token,
+  });
 };
 
 module.exports = {
